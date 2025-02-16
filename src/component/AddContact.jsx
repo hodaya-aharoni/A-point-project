4f18beb8e5e1edb6a8aa7545ef8cc5666f58f390
hodaya-aharoni <hh0527623715@gmail.com>
2025-02-16 04:08:15 +0200

    Fix display of names in edit mode

--- src/component/AddContact.jsx
+++ src/component/AddContact.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useForm, useFieldArray } from "react-hook-form";
-import { Box, Button, TextField, Select, MenuItem, Typography, Divider, IconButton, Accordion, AccordionSummary, AccordionDetails, ListItemIcon } from "@mui/material";
+import {
+    Box, Button, TextField, Select, MenuItem, Typography,
+    Divider, IconButton, Accordion, AccordionSummary, AccordionDetails, ListItemIcon
+} from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { useDispatch } from "react-redux";
@@ -21,15 +24,22 @@ const languages = [
     { code: "ar", label: "Arabic", flag: "🇸🇦" },
 ];
 
-const AddContact = ({ onClose, contact, isEdit}) => {
+const AddContact = ({ onClose, contact, isEdit }) => {
     const dispatch = useDispatch();
     const { register, control, handleSubmit, formState: { errors } } = useForm({
         defaultValues: {
-            firstName: "", lastName: "", role: "", contactType: "contractor",
-            language: "en",
-            phones: [{ number: "", type: "Work" }],
-            emails: [{ address: "", type: "Private" }],
-            address: "", comment: "", invoiceName: "", accountingRef: "", vatNumber: ""
+            firstName: isEdit ? contact.firstName : "" ,
+            lastName: isEdit ? contact.lastName : "",
+            role: isEdit ? contact.role : "",
+            contactType: isEdit ? contact.contactType : "contractor",
+            language: isEdit ? contact.language : "en",
+            phones: isEdit ? contact.phones : [{ number: "", type: "Work" }],
+            emails: isEdit ? contact.emails : [{ address: "", type: "Private" }],
+            address: isEdit ? contact.address : "",
+            comment: isEdit ? contact.comment : "",
+            invoiceName: isEdit ? contact.invoiceName : "",
+            accountingRef: isEdit ? contact.accountingRef : "",
+            vatNumber: isEdit ? contact.vatNumber : ""
         }
     });
 
@@ -45,17 +55,14 @@ const AddContact = ({ onClose, contact, isEdit}) => {
         <form onSubmit={handleSubmit(onSubmit)}>
             <Typography variant="h6" mb={2}>New Contact</Typography>
             <Box display="flex" gap={1} mb={2}>
-                <TextField label="First Name"
-                    {...register("firstName", { required: "Required" })}
-                    disabled={isEdit} error={!!errors.firstName} value={contact.firstName} fullWidth />
+                <TextField label="First Name" {...register("firstName", { required: "Required" })}
+                    error={!!errors.firstName} fullWidth />
                 <TextField label="Last Name" {...register("lastName", { required: "Required" })}
-                value={contact.lastName}
-                    disabled={isEdit} error={!!errors.lastName} fullWidth />
+                    error={!!errors.lastName} fullWidth />
             </Box>
             <Box display="flex" gap={1} mb={2}>
-                <TextField label="Role" {...register("role")} disabled={isEdit} value={contact.role} fullWidth />
-                <Select {...register("contactType")} defaultValue="contractor" value={contact.contactType} fullWidth>
-                    <MenuItem value="">   </MenuItem> 
+                <TextField label="Role" {...register("role")} fullWidth />
+                <Select {...register("contactType")} defaultValue="contractor" fullWidth>
                     <MenuItem value="Cleaner">Cleaner</MenuItem>
                     <MenuItem value="Gardener">Gardener</MenuItem>
                     <MenuItem value="Electrician">Electrician</MenuItem>
@@ -70,12 +77,11 @@ const AddContact = ({ onClose, contact, isEdit}) => {
                 <AccordionSummary expandIcon={<ExpandMoreIcon />}>Language & Contact</AccordionSummary>
                 <AccordionDetails>
                     <Box display="flex" gap={1} mb={2}>
-                        <Select {...register("language")} disabled={isEdit} value={contact.language} fullWidth>
+                        <Select {...register("language")} fullWidth>
                             {languages.map(lang => (
                                 <MenuItem key={lang.code} value={lang.code}>
                                     <ListItemIcon>{lang.flag}</ListItemIcon>
                                     {lang.label}
-                                 
                                 </MenuItem>
                             ))}
                         </Select>
@@ -84,45 +90,41 @@ const AddContact = ({ onClose, contact, isEdit}) => {
                     <Typography variant="subtitle1">Phone</Typography>
                     {phoneFields.map((field, index) => (
                         <Box display="flex" gap={1} alignItems="center" mb={1} key={field.id}>
-                            <TextField label="Phone" {...register(`phones.${index}.number`)} disabled={isEdit} value={contact.phones} fullWidth />
+                            <TextField label="Phone" {...register(`phones.${index}.number`)} fullWidth />
                             <Select {...register(`phones.${index}.type`)} defaultValue="Work">
-                                {phoneTypes.map(type => (
-                                    <MenuItem key={type} value={type}>{type}</MenuItem>
-                                ))}
+                                {phoneTypes.map(type => <MenuItem key={type} value={type}>{type}</MenuItem>)}
                             </Select>
                             <IconButton onClick={() => removePhone(index)}><DeleteIcon /></IconButton>
                         </Box>
                     ))}
-                    <Button onClick={() => addPhone({ number: "", type: "Work" })} disabled={isEdit}>+ Add Phone</Button>
+                    <Button onClick={() => addPhone({ number: "", type: "Work" })}>+ Add Phone</Button>
                     <Divider sx={{ my: 2 }} />
                     <Typography variant="subtitle1">Email</Typography>
                     {emailFields.map((field, index) => (
                         <Box display="flex" gap={1} alignItems="center" mb={1} key={field.id}>
-                            <TextField label="Email" {...register(`emails.${index}.address`)} disabled={isEdit} value={contact.emails} fullWidth />
+                            <TextField label="Email" {...register(`emails.${index}.address`)} fullWidth />
                             <Select {...register(`emails.${index}.type`)} defaultValue="Private">
-                                {emailTypes.map(type => (
-                                    <MenuItem key={type} value={type}>{type}</MenuItem>
-                                ))}
+                                {emailTypes.map(type => <MenuItem key={type} value={type}>{type}</MenuItem>)}
                             </Select>
                             <IconButton onClick={() => removeEmail(index)}><DeleteIcon /></IconButton>
                         </Box>
                     ))}
-                    <Button onClick={() => addEmail({ address: "", type: "Private" })} disabled={isEdit}>+ Add Email</Button>
+                    <Button onClick={() => addEmail({ address: "", type: "Private" })}>+ Add Email</Button>
                 </AccordionDetails>
             </Accordion>
             <Accordion>
                 <AccordionSummary expandIcon={<ExpandMoreIcon />}>Mailing Address</AccordionSummary>
                 <AccordionDetails>
-                    <TextField label="Address" {...register("address")} value={contact.address} fullWidth />
-                    <TextField label="Comment" {...register("comment")} fullWidth value={contact.comment}/>
+                    <TextField label="Address" {...register("address")} fullWidth />
+                    <TextField label="Comment" {...register("comment")} fullWidth />
                 </AccordionDetails>
             </Accordion>
             <Accordion>
                 <AccordionSummary expandIcon={<ExpandMoreIcon />}>Billing Information</AccordionSummary>
                 <AccordionDetails>
-                    <TextField label="Name of Invoice" {...register("invoiceName")} value={contact.address} fullWidth />
-                    <TextField label="Accounting Ref" {...register("accountingRef")} fullWidth value={contact.accountingRef} />
-                    <TextField label="VAT Number" {...register("vatNumber")} value={contact.vatNumber} fullWidth />
+                    <TextField label="Name of Invoice" {...register("invoiceName")} fullWidth />
+                    <TextField label="Accounting Ref" {...register("accountingRef")} fullWidth />
+                    <TextField label="VAT Number" {...register("vatNumber")} fullWidth />
                 </AccordionDetails>
             </Accordion>
             <Box display="flex" justifyContent="space-between" mt={3}>
